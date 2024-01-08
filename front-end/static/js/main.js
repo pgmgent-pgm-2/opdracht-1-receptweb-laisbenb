aa6e2f748de1b1a7d0eafe40e1b682b44a9fd71d
laisbenb <woodipsy@gmail.com>
2024-01-08 11:49:51 +0100

    all recipes fetched

--- front-end/static/js/main.js
+++ front-end/static/js/main.js
@@ -1,5 +1,5 @@
 async function getRecipes() {
-    const recipes = await fetch('http://localhost:4201/recipes');
+    let recipes = await fetch('http://localhost:4201/recipes');
     recipes = await recipes.json();
     console.log(recipes);
 };
