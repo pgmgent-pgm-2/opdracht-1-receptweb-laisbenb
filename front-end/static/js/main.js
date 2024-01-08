843986503210554785538e1aaa084243b465cb94
laisbenb <woodipsy@gmail.com>
2024-01-08 12:29:55 +0100

    genereteRecipes busy

--- front-end/static/js/main.js
+++ front-end/static/js/main.js
@@ -2,6 +2,19 @@ async function getRecipes() {
     let recipes = await fetch('http://localhost:4201/recipes');
     recipes = await recipes.json();
     console.log(recipes);
+    generateRecipes(recipes);
 };
 
 getRecipes();
+
+function generateRecipes(recipes) {
+    //title, cookingtime, difficulty, servings
+    const $recipe = document.getElementById('recipes');
+    let html = "";
+    for (const recipe in recipes) {
+        html = `<article>
+        <h2>${recipe.title}</h2>
+        </article>`
+    };
+    $recipe.innerHTML = html;
+};
