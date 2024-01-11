4c677629f79b1cca1f82d348ae178a454bc2d6e7
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-01-11 14:55:41 +0100

    recipe image added + little bit of css on main page

--- front-end/static/js/main.js
+++ front-end/static/js/main.js
@@ -12,7 +12,7 @@ function generateRecipes(recipes) {
     let html = "";
     for (const recipe in recipes) {
         console.log(recipe);
-        html += `<article>
+        html += `<article class="recipe-article">
         <h2>${recipes[recipe].title}</h2>
         <p>Cooking time: ${recipes[recipe].cookingTime}m</p>
         <p>Servings: ${recipes[recipe].servings} | Difficulty: ${recipes[recipe].difficulty}</p>
