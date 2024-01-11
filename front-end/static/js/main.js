073cbbb3eeaee808d16c1b63b63ae8902eea77a0
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-01-11 15:47:40 +0100

    css and js cleanup

--- front-end/static/js/main.js
+++ front-end/static/js/main.js
@@ -11,14 +11,17 @@ function generateRecipes(recipes) {
     const $recipe = document.getElementById('recipes');
     let html = "";
     for (const recipe in recipes) {
+        const recipeObj = recipes[recipe];
         //console.log(recipe);
         //console.log(recipes[recipe].image);
         html += `<article class="recipe-article">
-        <img src="./static/media/images/${recipes[recipe].image}" alt="${recipes[recipe].title}">
-        <h2>${recipes[recipe].title}</h2>
-        <p>Cooking time: ${recipes[recipe].cookingTime}m</p>
-        <p>Servings: ${recipes[recipe].servings} | Difficulty: ${recipes[recipe].difficulty}</p>
-        </article>`
+        <div class="image-box">
+        <img src="./static/media/images/${recipeObj.image}" alt="${recipeObj.title}">
+        </div>
+        <h2>${recipeObj.title}</h2>
+        <p>Cooking time: ${recipeObj.cookingTime}m</p>
+        <p>Servings: ${recipeObj.servings} | Difficulty: ${recipeObj.difficulty}</p>
+        </article>`;
     };
     $recipe.innerHTML = html;
 };
